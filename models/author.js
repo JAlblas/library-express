26a517d09b2b70eabd62603f0668d85e76077251
Jasper Alblas <just_jasper@hotmail.com>
2021-04-10 21:28:23 +0200

    author controller and view added

--- models/author.js
+++ models/author.js
@@ -1,5 +1,6 @@
-var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
@@ -22,7 +23,9 @@ AuthorSchema
 AuthorSchema
 .virtual('lifespan')
 .get(function () {
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  let deathDate = this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';;
+  let birthDate = this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';;
+  return (deathDate + " - " + birthDate);
 });
 
 // Virtual for author's URL
